5c3c7cde39ecdf4a269684817e43162216f94665
Lawrence Lian <LawrensuLeo@gmail.com>
2024-04-15 23:26:09 +0800

    Added the theme color for the website

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // Top being the least prioritised. Bottom being the most priortised.
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
+import '../styles/components/header.css';
 
 
 
