d0e531a8b7e6a4e6d4576f7d57bc245a87f4b910
Lawrence Lian <LawrensuLeo@gmail.com>
2024-04-18 13:50:49 +0800

    Added footer.css & home-page.css into the js file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
+import '../styles/components/footer.css';
+import '../styles/components/home-page.css';
 
 
 
