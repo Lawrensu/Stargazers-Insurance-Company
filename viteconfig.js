7710b199237acf48611442847e0cf6b9be2d4891
Lawrence Lian <LawrensuLeo@gmail.com>
2024-05-13 21:14:23 +0800

    Added all the html files needed

--- viteconfig.js
+++ viteconfig.js
@@ -1,5 +1,19 @@
 // vite.config.js
-export default {
-    base: '/Stargazers-Insurance-Company'
-    // config options
-  }
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default ({
+    base: '/Stargazers-Insurance-Company',
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          system: resolve(__dirname, 'system.html'),
+          agents: resolve(__dirname, 'agents.html'),
+          investors: resolve(__dirname, 'investors.html'),
+          contacts: resolve(__dirname, 'contacts.html'),
+          quotes: resolve(__dirname, 'quotes.html'),
+        },
+      },
+    },
+  });
